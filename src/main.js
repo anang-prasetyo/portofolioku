3319346472efd25940712f7d0d97c1ffc1bbafa3
L200180063 <anprasetyo060@gmail.com>
2023-06-21 11:10:03 +0700

    remove router

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
 import './bootstrap/css/bootstrap.css'
 import './bootstrap/font/bootstrap-icons.css'
@@ -8,6 +7,5 @@ import './assets/main.scss'
 
 const app = createApp(App)
 
-app.use(router)
 
 app.mount('#app')
