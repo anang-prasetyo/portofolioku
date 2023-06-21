4554b104ddc82707e4cfdb9ec2f65252bdd0a111
L200180063 <anprasetyo060@gmail.com>
2023-06-21 11:47:30 +0700

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/MyPortofolio/',
-  publicDir: '/MyPortofolio/',
+  base: '/MyPortofolio/',
   plugins: [
     vue(),
   ],
