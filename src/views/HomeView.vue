1d091a4db9c6508ce926c2d89a0a051bc3ab94ae
L200180063 <anprasetyo060@gmail.com>
2023-06-21 12:12:02 +0700

    remove myskill and myproject

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="d-flex flex-column" style="row-gap: 5rem;">
     <the-welcome />
-    <my-skills />
-    <my-projects />
+    <!-- <my-skills /> -->
+    <!-- <my-projects /> -->
     <!-- <my-proccess /> -->
     <div class="container">
       <div class="row">
@@ -20,8 +20,8 @@
 
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
-import MySkills from '../components/MySkills.vue'
-import MyProjects from '../components/MyProjects.vue'
+// import MySkills from '../components/MySkills.vue'
+// import MyProjects from '../components/MyProjects.vue'
 // import MyProccess from '../components/TheProccess.vue'
 
 </script>
